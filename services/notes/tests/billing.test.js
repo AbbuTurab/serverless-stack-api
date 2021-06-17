82fb367b93217862a448b3812dd75b5debd1e947
AbbuTurab <syed.abbuturab@fissionlabs.com>
2021-06-17 12:17:08 +0530

    Reorganizing the repo

--- services/notes/tests/billing.test.js
+++ services/notes/tests/billing.test.js
@@ -1,4 +1,4 @@
-import { calculateCost } from '../libs/billing-lib';
+import { calculateCost } from '../../../libs/billing-lib';
 test('Lowest tier',() => {
   const storage = 10;
   const cost = 4000;
